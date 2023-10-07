ef7e550384fc5b0738dbc796e5723e7982692bdf
victor.legac <victor.legac@eemi.com>
2023-10-07 19:12:23 +0200

    remove log

--- MXRIJUXNXMXFIX/app/urls.py
+++ MXRIJUXNXMXFIX/app/urls.py
@@ -3,13 +3,6 @@ from django.contrib import admin
 
 from . import views
 
-import logging
-
-
-logger = logging.getLogger("aled")
-
-logger.info(admin)
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage),
